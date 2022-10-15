e874d7726098b8d6c6fa009f38db994689bd4aa7
Rohit kumar <rohitkumarrr2022@gmail.com>
2022-10-15 17:18:34 +0530

    first commit

--- models/server.js
+++ models/server.js
@@ -1,14 +1,22 @@
 const mongoose = require("mongoose");
+const validator = require('validator');
 const schema = mongoose.Schema;
 
 let PlaylistSchema = new schema({
     name:{
         type:String,
-        required:true
+        required:true,
+        maxlength:3,
     },
     email:{
         type:String,
         required:true,
+        unique:[true, "Email ID is already present"],
+        validate(value){
+            if(!validator.isEmail(value)){
+                throw new Error("Invalid Email");
+            }
+        }
     }
 })
 
